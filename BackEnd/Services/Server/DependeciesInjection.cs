e4badee18be99a873a6df8908736329c58009cf9
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2024-08-31 08:49:28 -0300

    Organizando

---
+++ BackEnd/Services/Server/DependeciesInjection.cs
@@ -0,0 +1,16 @@
+﻿using ReserveiAPI.Repositories.Interfaces;
+using ReserveiAPI.Repositories.Entities;
+using ReserveiAPI.Services.Entities;
+using ReserveiAPI.Services.interfaces;
+namespace ReserveiAPI.Services.Server
+{
+	public static class DependeciesInjection
+	{
+		public static void AddUserDepencies(this IServiceCollection services)
+		{
+			//dependencia : usuario
+			services.AddScoped<IUserRepository, UserRepository>();
+				services.AddScoped<IUserService, UserService>();
+		}
+	}
+}
